9948c5513ed8a99fd6a96f5672f39898274c94a5
sachin vijwal <sachinvijwal@gmail.com>
2019-04-25 17:32:18 +0530

    init

---
+++ HotjarExample1/HotjarExample1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HotjarExample1.MvcApplication" Language="C#" %>
---
+++ HotjarExample1/HotjarExample1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotjarExample1.Startup))]
+namespace HotjarExample1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
